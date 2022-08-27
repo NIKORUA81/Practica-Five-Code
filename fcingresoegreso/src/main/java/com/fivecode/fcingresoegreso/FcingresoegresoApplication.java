115bf5e76275416194ba285788bf9a9c068fc54d
DiegoEdison <diegoedison05@gmail.com>
2022-08-26 20:20:27 -0500

    Intanciando clase Empresa, Empleado y Transaccion

--- fcingresoegreso/src/main/java/com/fivecode/fcingresoegreso/FcingresoegresoApplication.java
+++ fcingresoegreso/src/main/java/com/fivecode/fcingresoegreso/FcingresoegresoApplication.java
@@ -1,5 +1,8 @@
 package com.fivecode.fcingresoegreso;
 
+import com.fivecode.fcingresoegreso.entity.Empleado;
+import com.fivecode.fcingresoegreso.entity.Empresa;
+import com.fivecode.fcingresoegreso.entity.Transaccion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FcingresoegresoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FcingresoegresoApplication.class, args);
+		//SpringApplication.run(FcingresoegresoApplication.class, args);
+
+		//Creando una empresa
+		Empresa empresa1 = new Empresa(123, "Camiones SA", "Calle 24", "3101234567", "1128123000");
+		System.out.println("-----------Empresa-------------");
+		System.out.println(empresa1.getIdEmpresa());
+		System.out.println(empresa1.getNombre());
+		System.out.println(empresa1.getDocumento());
+		System.out.println(empresa1.getDireccion());
+		System.out.println(empresa1.getTelefono());
+
+
+
+		//Creando un empleado
+		Empleado empleado1 = new Empleado(101, "Diego", "Oviedo", "diego@gmail.com", 100456789);
+		System.out.println("-----------Empleado-------------");
+		System.out.println(empleado1.getIdEmpleado());
+		System.out.println(empleado1.getCedula());
+		System.out.println(empleado1.getNombre());
+		System.out.println(empleado1.getApellido());
+		System.out.println(empleado1.getCorreo());
+
+
+		//Creando una transancción
+		Transaccion transaccion1 = new Transaccion(0010, "Venta de algodon tipo A", 85000);
+		System.out.println("-----------Transacción-------------");
+		System.out.println(transaccion1.getIdTransaccion());
+		System.out.println(transaccion1.getDescripcion());
+		System.out.println(transaccion1.getMonto());
 	}
 
 }
