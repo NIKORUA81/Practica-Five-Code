3f25c0ba7a232f32d321ae0101f21d723e02d271
DiegoEdison <diegoedison05@gmail.com>
2022-08-28 20:17:42 -0500

    Cambiando jddk 18 por el jdk 11

---
+++ fcingresoegreso/src/test/java/com/fivecode/fcingresoegreso/FcingresoegresoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivecode.fcingresoegreso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcingresoegresoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
