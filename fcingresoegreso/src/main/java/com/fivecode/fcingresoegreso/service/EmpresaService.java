8f48dbf4fa4c4d9bbebd2b5d0a698d0fb9f46b45
DiegoEdison <diegoedison05@gmail.com>
2022-09-12 15:27:04 -0500

    Creación archivos EmpresaService

--- fcingresoegreso/src/main/java/com/fivecode/fcingresoegreso/service/EmpresaService.java
+++ fcingresoegreso/src/main/java/com/fivecode/fcingresoegreso/service/EmpresaService.java
@@ -1,5 +1,34 @@
 package com.fivecode.fcingresoegreso.service;
 
-public class EmpresaService {
+import com.fivecode.fcingresoegreso.entity.Empresa;
 
+import java.util.List;
+
+public class EmpresaService implements IEmpresaService{
+
+
+    @Override
+    public Empresa findById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Empresa> findAll() {
+        return null;
+    }
+
+    @Override
+    public Empresa createEmpresa(Empresa empresa) {
+        return null;
+    }
+
+    @Override
+    public Empresa updateEmpresa(long id, Empresa empresa) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmpresa(long id) {
+
+    }
 }
---
+++ fcingresoegreso/src/main/java/com/fivecode/fcingresoegreso/service/IEmpresaService.java
@@ -0,0 +1,20 @@
+package com.fivecode.fcingresoegreso.service;
+
+import com.fivecode.fcingresoegreso.entity.Empresa;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+public interface IEmpresaService {
+
+    public Empresa findById(long id);
+
+    public List<Empresa> findAll();
+
+    public Empresa createEmpresa(Empresa empresa);
+
+    public Empresa updateEmpresa(long id, Empresa empresa);
+
+    public void deleteEmpresa(long id);
+}
