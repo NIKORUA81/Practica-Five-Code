5d08d4c9f4f6b2dbf381844b0d544fc76d158573
DiegoEdison <diegoedison05@gmail.com>
2022-09-12 15:32:05 -0500

    Creación EmpleadoService

---
+++ fcingresoegreso/src/main/java/com/fivecode/fcingresoegreso/service/EmpleadoService.java
@@ -0,0 +1,32 @@
+package com.fivecode.fcingresoegreso.service;
+
+import com.fivecode.fcingresoegreso.entity.Empleado;
+
+import java.util.List;
+
+public class EmpleadoService implements IEmpleadoService{
+    @Override
+    public Empleado findById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Empleado> findAll() {
+        return null;
+    }
+
+    @Override
+    public Empleado createEmpleado(Empleado empleado) {
+        return null;
+    }
+
+    @Override
+    public Empleado updateEmpleado(long id, Empleado empleado) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmpleado(long id) {
+
+    }
+}
---
+++ fcingresoegreso/src/main/java/com/fivecode/fcingresoegreso/service/IEmpleadoService.java
@@ -0,0 +1,20 @@
+package com.fivecode.fcingresoegreso.service;
+
+import com.fivecode.fcingresoegreso.entity.Empleado;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+public interface IEmpleadoService {
+
+    public Empleado findById(long id);
+
+    public List<Empleado> findAll();
+
+    public Empleado createEmpleado(Empleado empleado);
+
+    public Empleado updateEmpleado(long id, Empleado empleado);
+
+    public void deleteEmpleado(long id);
+}
