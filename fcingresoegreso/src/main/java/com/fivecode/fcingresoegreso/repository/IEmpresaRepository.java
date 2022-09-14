3e9a51dfbbf151f022458199e08182b30a01615a
DiegoEdison <diegoedison05@gmail.com>
2022-09-14 18:31:26 -0500

    repository

---
+++ fcingresoegreso/src/main/java/com/fivecode/fcingresoegreso/repository/IEmpresaRepository.java
@@ -0,0 +1,8 @@
+package com.fivecode.fcingresoegreso.repository;
+
+import com.fivecode.fcingresoegreso.entities.Empresa;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IEmpresaRepository extends CrudRepository<Empresa, Long> {
+
+}
