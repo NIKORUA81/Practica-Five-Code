52a86c61359625bdb5aa9aa7a8635ffa6f8552b6
DiegoEdison <diegoedison05@gmail.com>
2022-09-15 14:53:26 -0500

    Agregrando la carpeta fcingresoegresothymeleaf

---
+++ fcingresoegresothymeleaf/src/main/java/com/fivecode/fcingresoegresothymeleaf/FcingresoegresothymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.fivecode.fcingresoegresothymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcingresoegresothymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcingresoegresothymeleafApplication.class, args);
+	}
+
+}
---
+++ fcingresoegresothymeleaf/src/test/java/com/fivecode/fcingresoegresothymeleaf/FcingresoegresothymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivecode.fcingresoegresothymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcingresoegresothymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
