8d04167a59dc2d733cbc7160050f0700b6c98b29
DiegoEdison <diegoedison05@gmail.com>
2022-09-28 21:41:07 -0500

    Ultimos arreglos

--- fcingresoegresothymeleaf/src/main/java/com/fivecode/fcingresoegresothymeleaf/util/EncriptarPassword.java
+++ fcingresoegresothymeleaf/src/main/java/com/fivecode/fcingresoegresothymeleaf/util/EncriptarPassword.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class EncriptarPassword {
 
     public static void main(String[] args) {
-        var password = "asd123";
+        var password = "1234";
         System.out.println("Password " + password);
         System.out.println("Password encriptado " + encriptarPassword(password));
     }
